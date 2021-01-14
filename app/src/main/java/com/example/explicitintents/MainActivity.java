72143e751d8fc157045abf2b609224289db04a2c
thomasfyllingen <Thomas.fyllingen@gmail.com>
2021-01-14 15:00:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/explicitintents/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.explicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int request_code =5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void askQuestion(View view){
+        Intent i = new Intent (  this, ActivityB.class);
+
+        final EditText editText = findViewById(R.id.editText1);
+        String myString = editText.getText().toString();
+        i.putExtra("qString", myString);
+        startActivityForResult(i, request_code);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+
+        if (( requestCode == request_code) &&
+                (resultCode == RESULT_OK)){
+            TextView textView1 = findViewById(R.id.textView1);
+            String returnString = data.getExtras().getString("returnData");
+            textView1.setText(returnString);
+        }
+    }
+
+
+}
